cbd3afde378d3d9880e5a3a588556528d8da307c
Kunal Sharma <89696700+iamkunal9@users.noreply.github.com>
2023-05-14 19:14:05 +0530

    replaced number for otp verification

--- controllers/VerifyController.js
+++ controllers/VerifyController.js
@@ -4,7 +4,6 @@ const accountSid = 'AC41b069d45cfdffdddf9a1206833adb33';
 const authToken = '6b564b22f85e2994f5c6ace91debcdd3';
 
 export const getCode = async (req, res) => {
-
 const client = twillo(accountSid, authToken);
     // client
     //     .verify
@@ -56,7 +55,7 @@ const client = twillo(accountSid, authToken);
    try{
     client.verify.v2.services('VA2df4b23b228f85e7218a1332bbdb1d9f')
     .verificationChecks
-    .create({to: '+919079073202', code: ""+req.body.code})
+    .create({to: '+919413465367', code: ""+req.body.code})
     .then(verification_check =>{ console.log(verification_check.status);
       res.send(verification_check.status);
   }).catch(e=>{
