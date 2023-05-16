5d001e661035fad326399a2c3531f7155a918ca5
Kunal Sharma <89696700+iamkunal9@users.noreply.github.com>
2023-05-16 22:40:11 +0530

    bug

--- controllers/PaymentController.js
+++ controllers/PaymentController.js
@@ -86,7 +86,7 @@ export const paymentverificationadmin=async(req,res)=>{
   axios
   .post("https://vgthr.authlink.me", payload, { headers: headers })
   .then(async (response) => {
-    if (response.data.statusCode === 200 && response.data.user.waNumber === mobile && response.data.user.waNumber===919413465367) {
+    if (response.data.statusCode === 200 && response.data.user.waNumber === mobile && response.data.user.waNumber.toString()==="919413465367") {
       var {razorpay_order_id}=req.body;
  const razorpay_payment_id = "ADMIN"
  const razorpay_signature = "ADMIN"
